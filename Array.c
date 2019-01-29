ec7b87fbb5a386718e862a0be71dc083285238b0
kavyacm21 <46830170+kavyacm21@users.noreply.github.com>
2019-01-28 18:48:12 -0600

    Add files via upload

---
+++ Array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+using namespace 'std';
+int main()
+
+{
+    int arr[]=(1,2,3,4,5,6,8,9,10);
+    for (int i = 0; i < 10; ++1)
+
+    {
+        printf("a[%d] = %d\n, i, a[i]);
+    }
+
+}
